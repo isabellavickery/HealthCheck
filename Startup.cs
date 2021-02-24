98ba18cd5398e62432283843fdc99869d7a60c1d
Isabella <Isabellafaithvickery@gmail.com>
2021-02-24 14:38:04 -0600

    Push

--- Startup.cs
+++ Startup.cs
@@ -27,8 +27,12 @@ namespace HealthCheck
                 configuration.RootPath = "ClientApp/dist";
             });
             services.AddHealthChecks()
-            .AddCheck<ICMPHealthCheck>("ICMP");
-
+            .AddCheck("ICMP_01", new ICMPHealthCheck("www.ryadel.com",
+            100))
+            .AddCheck("ICMP_02", new ICMPHealthCheck("www.google.com",
+            100))
+            .AddCheck("ICMP_03", new ICMPHealthCheck("www.does-not-exist.com",
+             100));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
